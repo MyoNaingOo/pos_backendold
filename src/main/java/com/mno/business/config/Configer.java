7fe67a3fa658265f94a62cda65cd4319d5c397aa
MyoNaingOo <myonaingoo623@gmail.com>
2023-12-29 22:38:57 +0630

    add frontend router

--- src/main/java/com/mno/business/config/Configer.java
+++ src/main/java/com/mno/business/config/Configer.java
@@ -74,7 +74,7 @@ public class Configer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200/", "http://localhost:3000/")
+                        .allowedOrigins("http://localhost:4200/", "http://localhost:3000/","http://localhost:8080/","http://localhost:5173/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE");
             }
         };
